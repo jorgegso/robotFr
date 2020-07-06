226058f583abae0e8a6d4e583ecf3fce9f79adaa
mikiimikii <hajorge2012@gmail.com>
2020-07-06 05:34:41 -0300

    Adding all the files.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
 import * as serviceWorker from './serviceWorker';
 import "tachyons"; // modulo de class de css 
-
-
-
 import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('root')
+// redux
+import { searchRobots, requestRobots } from './reducers';
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux';
+// redux-logger -- Middleware
+import { applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+
+// redux-thunk ------
+import thunkMiddleware from 'redux-thunk';
+
+
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store =
+  createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root')
 );
 serviceWorker.unregister();
