595391a6ea4687b087fa249cbf12a513e9299584
mikiimikii <hajorge2012@gmail.com>
2020-08-22 22:05:43 -0300

    Updates

---
+++ precache-manifest.6a25e0ac715ce43a0842059200898217.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247e6d693a203a87b28c08bdd02ebf8a",
+    "url": "/jorgegso/robofraindredux/index.html"
+  },
+  {
+    "revision": "5d7c9a031923becdc038",
+    "url": "/jorgegso/robofraindredux/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "367c671db6c33bac07f8",
+    "url": "/jorgegso/robofraindredux/static/css/main.449ecb88.chunk.css"
+  },
+  {
+    "revision": "5d7c9a031923becdc038",
+    "url": "/jorgegso/robofraindredux/static/js/2.e21a4731.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jorgegso/robofraindredux/static/js/2.e21a4731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367c671db6c33bac07f8",
+    "url": "/jorgegso/robofraindredux/static/js/main.ba0e2b69.chunk.js"
+  },
+  {
+    "revision": "1d660955cd3878f729ec",
+    "url": "/jorgegso/robofraindredux/static/js/runtime-main.d0423d14.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/jorgegso/robofraindredux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
